0866affba9d6ae8d1e5a76287aa46a333b73d3c3
Allan Chang <allanc1147@gmail.com>
2017-01-18 17:18:09 -0500

    Added an image for learning purposes.

--- src/employeeID/Main.java
+++ src/employeeID/Main.java
@@ -1,9 +1,11 @@
 package employeeID;
+import javax.imageio.*;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		test_image = ImageIO.read('hiddenLodge.jpg');
 
 	}
 

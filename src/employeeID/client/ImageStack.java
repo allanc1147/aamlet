0e71695d42946f0582eb96260f09056169447cd2
Piyush Datta <piyush.datta@mail.utoronto.ca>
2017-02-15 19:03:36 -0500

    Added a framework for ImageStack, will finish next meeting - Piyush, Feb 15 2017

--- src/employeeID/client/ImageStack.java
+++ src/employeeID/client/ImageStack.java
@@ -1,5 +1,16 @@
 package employeeID.client;
+import java.awt.image.BufferedImage;
+import java.util.Stack;
 
 public class ImageStack {
-
+	
+	// Create new Stack
+	Stack fifo = new Stack();
+	
+	// Insert new Image
+	BufferedImage new_image;
+	
+	public void pop(BufferedImage image){
+		
+	}
 }
